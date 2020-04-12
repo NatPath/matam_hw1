fbb924d0fbb2bd12906accd94db7dae8d1161dd5
Nativ Maor <nativ.maor@gmail.com>
2020-04-12 21:38:40 +0300

    removed redundancies

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nativ/Technion/matam/hw1/q1_home
-# Build directory: /home/nativ/Technion/matam/hw1/q1_home/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
