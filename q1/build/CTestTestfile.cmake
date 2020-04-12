641b74686ef0f1e43e3023d6688e12a6c6835f94
Nativ Maor <nativ.maor@gmail.com>
2020-04-12 22:07:42 +0300

    moved everything to nativ

--- q1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nativ/Technion/matam/hw1/q1_home/q1
-# Build directory: /home/nativ/Technion/matam/hw1/q1_home/q1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
