d43cfad3bc4f9f6e20eb38809e5c5b5a9926d958
Nativ Maor <nativ.maor@gmail.com>
2020-04-12 20:00:49 +0300

    "finished" dry part

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nativ/Technion/matam/hw1/q1_home
+# Build directory: /home/nativ/Technion/matam/hw1/q1_home/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
