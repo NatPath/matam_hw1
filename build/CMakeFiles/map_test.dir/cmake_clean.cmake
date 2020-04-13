4fe847a505862bc4654c3355a409ae24fcf83b70
Nativ Maor <nativ.maor@gmail.com>
2020-04-13 20:30:47 +0300

    from now on we debug

---
+++ build/CMakeFiles/map_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/map_test.dir/map/pairnode/pairnode.c.o"
+  "CMakeFiles/map_test.dir/map/map.c.o"
+  "CMakeFiles/map_test.dir/map/map_test.c.o"
+  "map_test.pdb"
+  "map_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/map_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
